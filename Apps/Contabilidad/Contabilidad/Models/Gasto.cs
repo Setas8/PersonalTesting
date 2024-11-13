e6b3610b66bbabc15cb437a8c63c0ed5b716171d
Diego <100022290+Setas8@users.noreply.github.com>
2024-11-13 21:49:34 +0100

    add models

---
+++ Apps/Contabilidad/Contabilidad/Models/Gasto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contabilidad.Models
+{
+    public class Gasto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public decimal Cantidad { get; set; }
+        public string Categoria { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+
+}
---
+++ Apps/Contabilidad/Contabilidad/Models/Recibo.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contabilidad.Models
+{
+    public class Recibo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Cantidad { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public string Frecuencia { get; set; } // Ejemplo: "Mensual", "Anual"
+    }
+}
