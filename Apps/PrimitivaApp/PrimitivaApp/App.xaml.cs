1e27a6322784dcfd07a886a3a6a492a2ff9c3d2f
Diego <100022290+Setas8@users.noreply.github.com>
2024-12-07 12:03:10 +0100

    add primitivaApp

---
+++ Apps/PrimitivaApp/PrimitivaApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PrimitivaApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Apps/PrimitivaApp/PrimitivaApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PrimitivaApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
